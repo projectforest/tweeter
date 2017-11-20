6597c98250dbe88ec0aff3f283f4291e7acbaf9f
hlinforest <hllin@sfu.ca>
2017-11-20 12:37:28 -0800

    implemented function to refetch tweets on submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,81 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
- 
-function timeSince(date) {
-
-    const seconds = Math.floor((new Date() - date) / 1000);
-
-    let interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-        return interval + " years ago";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-}
-
-const data = [
-  {
-    "user": {
-      "name": "Isaac Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
 $(document).ready( () => {
 
@@ -88,8 +13,7 @@ $(document).ready( () => {
   }
 
   function createTweetElement(tweet) {
-    // let created_at = new Date(tweet.created_at).toString().split(' ').slice(0, 4).join(' ');
-    let created_at = timeSince(tweet.created_at);
+    let created_at = new Date(tweet.created_at).toString().split(' ').slice(0, 4).join(' ');
     let header = '<header>' +
                     '<img src="' + escape(tweet.user.avatars.small) + '" />' +
                     '<h1>' + escape(tweet.user.name) + '</h1>' +
@@ -101,7 +25,6 @@ $(document).ready( () => {
     let iconHTML = icons.map(icon => {
       return '<i class="fa fa-' + icon + '" aria-hidden="true"></i>'
     }).join(' ');
-
     let footer = '<footer>' +
                     escape(created_at)  + '<span>' +
                     iconHTML;
@@ -110,10 +33,45 @@ $(document).ready( () => {
   }
 
   function renderTweets(tweets) {
-    for(tweet of tweets) {
+    for(let tweet of tweets) {
       $('.tweets').append(createTweetElement(tweet));
     }
   }
+  function loadTweets() {
+    $.getJSON('tweets', function(tweets) {
+      renderTweets(tweets);
+    });
+  }
 
-  renderTweets(data);
+  loadTweets();
+
+  function submitHandler() {
+    $('.new-tweet input[type="submit"]').on('click', function (event) {
+      event.preventDefault();
+
+      let form = $(this).closest('form');
+      let input = form.find('textarea');
+      let error = form.find('p');
+
+      // removes error element if present
+      if(error.length) {
+        error.remove();
+      }
+
+      if(input.val() === '') {
+        $(this).after('<p>Error: Input cannot be empty</p>');
+      } else if(140 - input.val().length < 0) {
+        $(this).after('<p>Error: Input exceeds 140 characters</p>');
+      } else {
+        $.post('tweets', input.serialize(), function(tweet) {
+          $('.tweets').prepend(createTweetElement(tweet));
+          input.val('');
+        });
+      }
+
+    });
+  }
+
+  submitHandler();
+
 });
--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,9 +1,24 @@
+
 $(document).ready(function() {
   $('.new-tweet input[type="submit"]').on('click', function (event) {
     event.preventDefault();
-    let data = $(this).closest('form').find('textarea').serialize();
-    $.post('tweets', data, function(res) {
-      console.log(res);
-    });
+
+    let form = $(this).closest('form');
+    let input = form.find('textarea');
+    let error = form.find('p');
+
+    // removes error if present
+    if(error.length) {
+      error.remove();
+    }
+
+    if(input.val() === '') {
+      $(this).after('<p>Error: Input cannot be empty</p>');
+    } else if(140 - input.val().length < 0) {
+      $(this).after('<p>Error: Input exceeds 140 characters</p>');
+    } else {
+      $.post('tweets', input.serialize());
+    }
+
   });
 });
